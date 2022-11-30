b9a66ebaea6994875e45f8f6b796c4b7a8be81da
Justine S <jstalzer@ufl.edu>
2022-11-29 22:11:49 -0500

    Added stuff

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     cout << "Hello World\n";
-    cout<< "Justine learns Git!!!!";
+    cout<< "Justine learns Git!!!!\n";
+    cout << "Chirag teaches Justine Git!!!!"; 
     return 0;
 }
