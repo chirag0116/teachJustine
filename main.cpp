d497bf72a08021031e2a7a053c3548c240a0a74a
chirag0116 <cnarang@ufl.edu>
2022-11-29 22:17:09 -0500

    added whitespace

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main(){
     cout << "Hello World\n";
+    cout << endl;
     cout<< "Justine learns Git!!!!\n";
+    cout << endl;
     cout << "Chirag teaches Justine Git!!!!"; 
+    cout << endl;
     return 0;
 }
